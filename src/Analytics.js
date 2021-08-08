b491535bc9c31e9f486fe4ab047e1c1f42515996
Austin Chabaud <austinc1023@yahoo.com>
2021-08-07 22:04:57 -0500

    updated env

--- src/Analytics.js
+++ src/Analytics.js
@@ -4,7 +4,7 @@ function Analytics() {
 	useEffect(() => {
 		ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_CODE);
 
-		ReactGA.pageview(window.location.pathname);
+		ReactGA.pageview(window.location.pathname + window.location);
 	}, []);
 	return <div></div>;
 }
